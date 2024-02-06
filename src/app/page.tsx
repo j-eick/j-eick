b3f5a9af673ca2d4c787564d5c358fff3e791ca8
Joschka Eickhoff <jeickhoff@posteo.de>
2024-02-06 22:50:48 +0100

    basic design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,60 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import "./globals.css";
+import projects from "../../utils/projects";
+
+console.clear();
 
 export default function Home() {
   return (
-    <main className={`${styles.main} ${styles.main__border} container row`}>
-      <section className={styles.section}>
-        <article className={`flow`}>
-          <h1>Hi, my name is ...</h1>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
-            repellat animi mollitia culpa nam recusandae excepturi explicabo at
-            aliquid facere odit, vel a nemo beatae voluptates voluptas.
-            Dignissimos, ullam perspiciatis.
-          </p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
-            repellat animi mollitia culpa nam recusandae excepturi explicabo at
-            aliquid facere odit, vel a nemo beatae voluptates voluptas.
-            Dignissimos, ullam perspiciatis.
-          </p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
-            repellat animi mollitia culpa nam recusandae excepturi explicabo at
-            aliquid facere odit, vel a nemo beatae voluptates voluptas.
-            Dignissimos, ullam perspiciatis.
-          </p>
-        </article>
-      </section>
-      <aside className={`${styles.aside}`} style={{ border: "2px solid red" }}>
-        <ul role="list" className={styles.uList}>
-          <li className="tile" style={{ backgroundColor: "darkred" }}>
-            project
+    <>
+      <main className={`${styles.main} container flex_row`}>
+        <section className={styles.section}>
+          <article className={`flow`}>
+            <h1>Hi, my name is ...</h1>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+              repellat animi mollitia culpa nam recusandae excepturi explicabo
+              at aliquid facere odit, vel a nemo beatae voluptates voluptas.
+              Dignissimos, ullam perspiciatis.
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+              repellat animi mollitia culpa nam recusandae excepturi explicabo
+              at aliquid facere odit, vel a nemo beatae voluptates voluptas.
+              Dignissimos, ullam perspiciatis.
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+              repellat animi mollitia culpa nam recusandae excepturi explicabo
+              at aliquid facere odit, vel a nemo beatae voluptates voluptas.
+              Dignissimos, ullam perspiciatis.
+            </p>
+          </article>
+        </section>
+        <aside className={`${styles.aside}`}>
+          <ul role="list" className={`flex_col ${styles.ul}`}>
+            {projects.map((item) => (
+              <li key={item.id} className={`tile ${styles.tile}`}>
+                <img src={item.thumbn} alt={item.alt} />
+              </li>
+            ))}
+          </ul>
+        </aside>
+      </main>
+      <div>
+        <ul className={`flex_row container`} role="list">
+          <li>
+            <p>LinkedIn</p>
           </li>
-          <li className="tile" style={{ backgroundColor: "green" }}>
-            project
+          <li>
+            <p>github</p>
           </li>
-          <li className="tile" style={{ backgroundColor: "orange" }}>
-            project
-          </li>
-          <li className="tile" style={{ backgroundColor: "brown" }}>
-            project
-          </li>
-          <li className="tile" style={{ backgroundColor: "violet" }}>
-            project
+          <li>
+            <p>sumthin</p>
           </li>
         </ul>
-      </aside>
-    </main>
+      </div>
+    </>
   );
 }
